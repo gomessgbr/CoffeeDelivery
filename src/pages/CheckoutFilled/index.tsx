195c2e5ebd79be4838d63e9008384176809b424f
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 09:55:32 -0300

    refactor: change name page

--- src/pages/CheckoutFilled/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CheckoutFilledContainer } from './styles'
-
-export function CheckoutFilled() {
-  return (
-    <CheckoutFilledContainer>
-      <div>CheckFilled</div>
-    </CheckoutFilledContainer>
-  )
-}
---
+++ src/pages/Filled/index.tsx
@@ -0,0 +1,9 @@
+import { FilledContainer } from './styles'
+
+export function Filled() {
+  return (
+    <FilledContainer>
+      <div>CheckFilled</div>
+    </FilledContainer>
+  )
+}
