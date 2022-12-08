9630ac473b10b14e0d5e964b04249ecdf80a469b
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 10:33:24 -0300

    style: Header Component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.div``
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-height: 6.5rem;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 10rem;
+`
+
+export const LocaleContainer = styled.div``
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 90rem;
+  display: flex;
+  flex-direction: column;
 `
