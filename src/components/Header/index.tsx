ca09db55356f12a774a948c05bfe3a3753c83d67
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 10:20:40 -0300

    feat: icons from phosphor and content of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
-import { HeaderContainer } from './styles'
+import { HeaderContainer, LocaleContainer } from './styles'
 import coffeDeliveryLogo from '../../assets/CoffeeDeliveryLogo.svg'
+import { NavLink } from 'react-router-dom'
+import { ShoppingCart, MapPin } from 'phosphor-react'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={coffeDeliveryLogo} alt="" />
-      
+      <nav>
+        <LocaleContainer>
+          <MapPin size={20} />
+          São Paulo,SP
+        </LocaleContainer>
+        <NavLink to="Checkout" title="carrinho">
+          <ShoppingCart size={24} />
+        </NavLink>
+      </nav>
     </HeaderContainer>
   )
 }
