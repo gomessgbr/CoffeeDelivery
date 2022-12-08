9e8ec823ac7f5c8c003db1ce7d5a30604a1e1268
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 09:28:54 -0300

    feat: CheckoutFilled component"

---
+++ src/pages/CheckoutFilled/index.tsx
@@ -0,0 +1,9 @@
+import { CheckoutFilledContainer } from './styles'
+
+export function CheckoutFilled() {
+  return (
+    <CheckoutFilledContainer>
+      <div></div>
+    </CheckoutFilledContainer>
+  )
+}
