e2b750a3abf7ce99b1948ebe43187ad38c7d46b0
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-09 07:54:27 -0300

    style: home style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 34rem;
+  align-items: center;
+  justify-content: center;
+  left: 5.5rem;
+  right: -5.5rem;
+  top: 8.0625rem;
+`
+
+export const HomeHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 1rem;
+  width: 36.75rem;
+`
+
+export const HomeTitle = styled.text`
+  font-family: 'Baloo 2';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 3rem;
+  line-height: 3.875rem;
+`
+
+export const HomeHeaderTitle = styled.div`
+  background-color: aquamarine;
+`
+
+export const HomeHeaderSubTitle = styled.div``
+
+export const HomeHeaderImg = styled.img``
