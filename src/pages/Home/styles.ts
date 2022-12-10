19167613fa46fc4b8bb58cb4af42ae8f58da8e69
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-10 12:08:06 -0300

    style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 export const HomeContainer = styled.div`
   display: flex;
   width: 100%;
-  height: 34rem;
+  max-height: 34rem;
   align-items: center;
   justify-content: center;
+  margin: 0 5.5rem;
+  padding: 0;
   left: 5.5rem;
   right: -5.5rem;
   top: 8.0625rem;
@@ -16,8 +18,17 @@ export const HomeHeader = styled.div`
   flex-direction: column;
   align-items: flex-start;
   padding: 0px;
-  gap: 1rem;
+`
+
+export const HomeHeaderTitle = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 36.75rem;
+  height: 12rem;
+  gap: 1rem;
+  left: calc(50% - 36.75rem / 2 - 16.625rem);
+  top: calc(50% - 12rem / 2 - 5.125rem);
+  background-color: aquamarine;
 `
 
 export const HomeTitle = styled.text`
@@ -26,10 +37,6 @@ export const HomeTitle = styled.text`
   font-weight: 800;
   font-size: 3rem;
   line-height: 3.875rem;
-`
-
-export const HomeHeaderTitle = styled.div`
-  background-color: aquamarine;
 `
 
 export const HomeHeaderSubTitle = styled.div``
