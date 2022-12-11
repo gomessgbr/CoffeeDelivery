9d5cfcff04a0d0800dff56a897e746a328164fc7
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-11 00:55:23 -0300

    fix

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,9 @@ export const GlobalStyle = createGlobalStyle`
     *{
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
+        box-sizing: border-box;\
+
+        
     }
 
 
