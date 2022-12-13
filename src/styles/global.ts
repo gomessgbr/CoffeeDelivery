417e0492bef6a56e3ca698aa59310adb88fdb500
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-13 09:00:05 -0300

    style: global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,20 +6,19 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-
-        
+        outline: none;
     }
 
 
 
 body{
-    background: ${({ theme }) => theme['white-100']};
-    max-width: 90rem;
-
+    background: ${({ theme }) => theme.colors['white-100']};
+    color: ${({ theme }) => theme.colors['gray-600']};  
     -webkit-font-smoothing: antialiased
 }
     body, input, button, text{
-        font-family: 'Roboto', sans-serif;
+        font-family: ${({ theme }) => theme.fonts.regular};
+        font-weight: 400;
        
     }
 
