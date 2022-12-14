a3bab3e644ea3b2991a2d6b63c6382db5e802cdf
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-14 16:02:34 -0300

    feat: componenting Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,53 +1,13 @@
-import {
-  HomeContainer,
-  HomeHeaderImg,
-  HomeHeaderSubTitle,
-  HomeHeaderTitle,
-  HomeHeader,
-  HomeTitle,
-} from './styles'
+import { HomeContainer } from './styles'
 
-import coffeImg from '../../assets/navHome.svg'
-import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { CoffeeCard } from './components/CoffeCards'
+import { HeaderHome } from './components/HeaderHome'
 
 export function Home() {
   return (
     <HomeContainer>
-      <HomeHeader>
-        <HomeHeaderTitle>
-          <HomeTitle>
-            Encontre o café perfeito para qualquer hora do dia
-          </HomeTitle>
-          <span>
-            Com o Coffee Delivery você recebe seu café onde estiver, a {'\n'}
-            qualquer hora
-          </span>
-        </HomeHeaderTitle>
-        <HomeHeaderSubTitle>
-          <span>
-            <ShoppingCart className="shopIcon" size={20} />
-            Compra simples e segura
-          </span>
-
-          <span>
-            <Timer className="timerIcon" size={20} />
-            Entrega rápida e rastreada
-          </span>
-
-          <span>
-            <Package className="packageIcon" size={20} />
-            Embalagem mantém o café intacto
-          </span>
-
-          <span>
-            <Coffee className="coffeeIcon" size={20} />O café chega fresquinho
-            até você
-          </span>
-        </HomeHeaderSubTitle>
-      </HomeHeader>
-      <div>
-        <HomeHeaderImg src={coffeImg} alt="" />
-      </div>
+      <HeaderHome />
+      <CoffeeCard />
     </HomeContainer>
   )
 }
