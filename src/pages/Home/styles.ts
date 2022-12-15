c39e954784f879b29936c4308197160882124b83
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-15 09:37:34 -0300

    style: HomeTitle styled component

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,11 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div``
-export const HomeMain = styled.main``
+export const HomeTitle = styled.p`
+  margin-left: 10rem;
+  font-family: 'Baloo 2';
+  font-size: 2rem;
+  font-weight: 800;
+  line-height: 2.6rem;
+  color: ${({ theme }) => theme.colors['gray-700']};
+`
