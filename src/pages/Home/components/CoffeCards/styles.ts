6f96eb5cca0292217c75d0ea4aca03655a61e438
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-16 16:08:19 -0300

    style: add style on coffee cards

--- src/pages/Home/components/CoffeCards/styles.ts
+++ src/pages/Home/components/CoffeCards/styles.ts
@@ -2,4 +2,20 @@ import styled from 'styled-components'
 
 export const CoffeeCardContainer = styled.div`
   display: flex;
+  width: 100%;
+  background-color: #6123;
 `
+
+export const CoffeeContent = styled.div`
+  display: flex;
+  background: ${({ theme }) => theme.colors['gray-100']};
+  width: 16rem;
+  height: 19.4rem;
+
+  flex-wrap: wrap;
+  margin-bottom: 1rem;
+`
+
+export const CoffeeCardDescription = styled.div``
+
+export const CoffeCardFooter = styled.div``
