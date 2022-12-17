f1ab1105c762a878fc4b1db41e651358d617c1d9
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-17 11:58:46 -0300

    style: new components styles

--- src/pages/Home/components/CoffeCards/styles.ts
+++ src/pages/Home/components/CoffeCards/styles.ts
@@ -2,20 +2,31 @@ import styled from 'styled-components'
 
 export const CoffeeCardContainer = styled.div`
   display: flex;
-  width: 100%;
-  background-color: #6123;
-`
-
-export const CoffeeContent = styled.div`
-  display: flex;
   background: ${({ theme }) => theme.colors['gray-100']};
   width: 16rem;
   height: 19.4rem;
-
-  flex-wrap: wrap;
-  margin-bottom: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 2rem 2.5rem 0;
 `
 
-export const CoffeeCardDescription = styled.div``
+export const CoffeeCardDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-export const CoffeCardFooter = styled.div``
+  span {
+    text-align: center;
+    margin: 0.5rem;
+  }
+`
+
+export const CoffeCardFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1rem;
+`
+
+export const PlusButton = styled.button``
+export const MinusButton = styled.button``
