cee6d7a25eca5684e876b925d12e1ce16cf6cd03
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-17 11:45:58 -0300

    style: adjusts on HomeContent style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 export const HomeTitle = styled.p`
   margin-left: 10rem;
   font-family: 'Baloo 2';
@@ -8,4 +11,14 @@ export const HomeTitle = styled.p`
   font-weight: 800;
   line-height: 2.6rem;
   color: ${({ theme }) => theme.colors['gray-700']};
+`
+
+export const HomeContent = styled.div`
+  display: flex;
+  height: 86rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  margin: 3.375rem 10rem 10rem 10rem;
 `
