f300c635ad70c3f425137b77211e760729202378
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-18 09:09:17 -0300

    feat: map on cofffes array

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,26 +3,17 @@ import { HomeContainer, HomeContent, HomeTitle } from './styles'
 import { CoffeeCard } from './components/CoffeCards'
 import { HeaderHome } from './components/HeaderHome'
 
+import { coffees } from '../../data/coffees'
+
 export function Home() {
   return (
     <HomeContainer>
       <HeaderHome />
       <HomeTitle>Nossos Cafés</HomeTitle>
       <HomeContent>
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
+        {coffees.map((coffe) => (
+          <CoffeeCard key={coffe.id} />
+        ))}
       </HomeContent>
     </HomeContainer>
   )
