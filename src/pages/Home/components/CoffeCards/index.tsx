481abd8efdee5a26eb80de0d76bac665e7dfe1d0
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-19 18:10:22 -0300

    feat: interface

--- src/pages/Home/components/CoffeCards/index.tsx
+++ src/pages/Home/components/CoffeCards/index.tsx
@@ -5,26 +5,40 @@ import {
   CoffeCardFooter,
   PlusButton,
   MinusButton,
+  Tags,
 } from './styles'
+
+export interface CoffeeProps {
+  id: string
+  tags: string[]
+  name: string
+  price: string
+  description: string
+  photo: string
+}
 
 export function CoffeeCard() {
   return (
     <CoffeeCardContainer>
       <CoffeeCardDescription>
         <img src="/CoffeesImg/tradicional.png" alt="" />
-        <span>Tradicional</span>
+        <Tags>
+          <span>Tradicional</span>
+        </Tags>
         <span>Expresso tradicional</span>
         <span>O tradicional café feito com água quente e grãos moídos</span>
       </CoffeeCardDescription>
       <CoffeCardFooter>
-        <span>R$ 9,90</span>
+        <p>R$</p> <h1>9,90</h1>
         <div>
           <MinusButton>-</MinusButton>
           <span>1</span>
 
           <PlusButton>+</PlusButton>
         </div>
-        <ShoppingCart size={20} weight="fill" />
+        <button>
+          <ShoppingCart size={20} weight="fill" />
+        </button>
       </CoffeCardFooter>
     </CoffeeCardContainer>
   )
