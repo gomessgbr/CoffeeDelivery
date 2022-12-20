11de77e945bdbe83e60ad7a3f27d191a2c1aac4c
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 12:45:28 -0300

    style: on card tags

--- src/pages/Home/components/CoffeCards/styles.ts
+++ src/pages/Home/components/CoffeCards/styles.ts
@@ -3,12 +3,20 @@ import styled from 'styled-components'
 export const CoffeeCardContainer = styled.div`
   display: flex;
   background: ${({ theme }) => theme.colors['gray-100']};
-  width: 16rem;
-  height: 19.4rem;
+  width: 100%;
+
+  border-radius: 6px 36px 6px 36px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 0 2rem 2.5rem 0;
+  padding: 1.25rem;
+  padding-top: 0;
+  img {
+    width: 7.5rem;
+    height: 7.5rem;
+    margin-top: -1.25rem;
+  }
 `
 
 export const CoffeeCardDescription = styled.div`
@@ -30,3 +38,23 @@ export const CoffeCardFooter = styled.div`
 
 export const PlusButton = styled.button``
 export const MinusButton = styled.button``
+
+export const Tags = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  margin-top: 1rem;
+  margin-bottom: 1.25rem;
+  flex-wrap: wrap;
+  span {
+    background: ${({ theme }) => theme.colors['yellow-100']};
+    color: ${({ theme }) => theme.colors['yellow-300']};
+    font-size: 'Roboto';
+    text-transform: uppercase;
+    padding: 0.25rem 0.5rem;
+    border-radius: 999px;
+    font-weight: 700;
+  }
+`
