b4261b543e858abfbfdb93742f576e18dc8502f1
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 13:41:30 -0300

    refactor: create a new HomeComponent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,14 @@
-import { HomeContainer, HomeContent, HomeTitle } from './styles'
+import { HomeContainer } from './styles'
 
-import { CoffeeCard } from './components/CoffeCards'
 import { HeaderHome } from './components/HeaderHome'
 
-import { coffees } from '../../data/coffees'
+import { OurCoffees } from './components/OurCoffee'
 
 export function Home() {
   return (
     <HomeContainer>
       <HeaderHome />
-      <HomeTitle>Nossos Cafés</HomeTitle>
-      <HomeContent>
-        {coffees.map((coffe) => (
-          <CoffeeCard key={coffe.id} />
-        ))}
-      </HomeContent>
+      <OurCoffees />
     </HomeContainer>
   )
 }
