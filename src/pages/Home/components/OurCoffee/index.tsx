5d08c71583038f0044e0e9de1bdf8b69e6cfd112
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 13:42:24 -0300

    feat: OurCoffee Component to list coffees

---
+++ src/pages/Home/components/OurCoffee/index.tsx
@@ -0,0 +1,18 @@
+import { coffees } from '../../../../data/coffees'
+import { CoffeeCard } from '../CoffeCards'
+
+import { CoffeeList, OurCoffeesContainer, HomeTitle } from './styles'
+
+export function OurCoffees() {
+  return (
+    <OurCoffeesContainer className="container">
+      <HomeTitle>Nossos Cafés</HomeTitle>
+
+      <CoffeeList>
+        {coffees.map((coffee) => {
+          return <CoffeeCard key={coffee.id} />
+        })}
+      </CoffeeList>
+    </OurCoffeesContainer>
+  )
+}
