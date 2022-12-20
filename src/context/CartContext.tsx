517d82fbb1357541d37f254cc1d4723c26cc3aa1
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 18:36:29 -0300

    feat: create a CartContext

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,112 @@
+import produce from 'immer'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { CoffeeInt } from '../pages/Home/components/CoffeCards'
+
+export interface CartItem extends CoffeeInt {
+  amount: number
+}
+
+interface CartContextType {
+  cartItems: CartItem[]
+  cartQuantity: number
+  cartItemsTotal: number
+  addCoffeeToCart: (coffee: CartItem) => void
+  changeCartItem: (cartItemId: string, type: 'increase' | 'decrease') => void
+  removeCartItem: (cartItemId: string) => void
+  cleanCart: () => void
+}
+
+interface CartContextProviderProps {
+  children: ReactNode
+}
+const COFFEE_ITEMS_STORAGE_KEY = 'coffeeDelivery:cartItems'
+
+export const CartContext = createContext({} as CartContextType)
+
+export function CardContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = localStorage.getItem(COFFEE_ITEMS_STORAGE_KEY)
+
+    if (storedCartItems) {
+      return JSON.parse(storedCartItems)
+    } else {
+      return []
+    }
+  })
+
+  const cartQuantity = cartItems.length
+
+  const cartItemsTotal = cartItems.reduce((total, cartItem) => {
+    return total + cartItem.price * cartItem.amount
+  }, 0)
+
+  function addCoffeeToCart(coffee: CartItem) {
+    const coffeeAlreadyExistsInCart = cartItems.findIndex(
+      (cartItem) => cartItem.id === coffee.id,
+    )
+
+    const newCart = produce(cartItems, (draft) => {
+      if (coffeeAlreadyExistsInCart < 0) {
+        draft.push(coffee)
+      } else {
+        draft[coffeeAlreadyExistsInCart].amount += coffee.amount
+      }
+    })
+
+    setCartItems(newCart)
+  }
+
+  function changeCartItem(cartItemId: string, type: 'increase' | 'decrease') {
+    const newCart = produce(cartItems, (draft) => {
+      const coffeeExistsInCart = cartItems.findIndex(
+        (cartItem) => cartItem.id === cartItemId,
+      )
+
+      if (coffeeExistsInCart >= 0) {
+        const item = draft[coffeeExistsInCart]
+        draft[coffeeExistsInCart].amount =
+          type === 'increase' ? item.amount + 1 : item.amount - 1
+      }
+    })
+
+    setCartItems(newCart)
+  }
+
+  function removeCartItem(cartItemId: string) {
+    const newCart = produce(cartItems, (draft) => {
+      const coffeeExistsInCart = cartItems.findIndex(
+        (cartItem) => cartItem.id === cartItemId,
+      )
+
+      if (coffeeExistsInCart >= 0) {
+        draft.splice(coffeeExistsInCart, 1)
+      }
+    })
+
+    setCartItems(newCart)
+  }
+
+  function cleanCart() {
+    setCartItems([])
+  }
+
+  useEffect(() => {
+    localStorage.setItem(COFFEE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems))
+  }, [cartItems])
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        cartQuantity,
+        addCoffeeToCart,
+        changeCartItem,
+        removeCartItem,
+        cartItemsTotal,
+        cleanCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
