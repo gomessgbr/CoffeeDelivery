ab1e91ded79576e10097afc375a052a3ef19d27c
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 13:30:45 -0300

    style: amount component

--- src/components/Amount/styles.ts
+++ src/components/Amount/styles.ts
@@ -1,1 +1,57 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface AmountInputContainerProps {
+  size?: 'medium' | 'small'
+}
+
+export const AmountInputContainer = styled.div<AmountInputContainerProps>`
+  flex: 1;
+  background: ${({ theme }) => theme.colors['gray-300']};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 4px;
+  border-radius: 6px;
+  padding: 0.5rem 0.531rem;
+
+  input {
+    text-align: center;
+    width: 100%;
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.colors['gray-800']};
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  ${({ size }) =>
+    size === 'medium' &&
+    css`
+      padding: 0.5rem;
+    `}
+
+  ${({ size }) =>
+    size === 'small' &&
+    css`
+      padding: 0.3rem 0.5rem;
+    `}
+`
+
+export const IconWrapper = styled.button.attrs({
+  type: 'button',
+})`
+  width: 0.875rem;
+  height: 0.875rem;
+  border: none;
+  background: none;
+  color: ${({ theme }) => theme.colors['purple-100']};
+  transition: 0.4s;
+  &:disabled {
+    opacity: 0.4;
+  }
+  &:not(:disabled):hover {
+    color: ${({ theme }) => theme.colors['purple-300']};
+  }
+`
