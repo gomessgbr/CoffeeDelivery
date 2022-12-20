8be1119a0acac02a0473cb0eb8b1f8ef31952360
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-20 13:15:02 -0300

    feat: amount interface props

--- src/components/Amount/index.tsx
+++ src/components/Amount/index.tsx
@@ -1,1 +1,28 @@
-export function Amount() {}
+import { AmountInputContainer, IconWrapper } from './styles'
+import { Minus, Plus } from 'phosphor-react'
+
+interface AmountProps {
+  size?: 'medium' | 'small'
+  amount: number
+  onIncrease: () => void
+  onDecrease: () => void
+}
+
+export function Amount({
+  amount,
+  onDecrease,
+  onIncrease,
+  size = 'medium',
+}: AmountProps) {
+  return (
+    <AmountInputContainer size={size}>
+      <IconWrapper disabled={amount <= 1} onClick={onDecrease}>
+        <Minus size={14} weight="fill" />
+      </IconWrapper>
+      <input type="number" readOnly value={amount} />
+      <IconWrapper onClick={onIncrease}>
+        <Plus size={14} weight="fill" />
+      </IconWrapper>
+    </AmountInputContainer>
+  )
+}
