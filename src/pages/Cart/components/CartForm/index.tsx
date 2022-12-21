2b9ab57ebe02eb64525f4729a9468e32631de5f5
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-21 17:02:16 -0300

    feat: inputs on cart

--- src/pages/Cart/components/CartForm/index.tsx
+++ src/pages/Cart/components/CartForm/index.tsx
@@ -1,5 +1,6 @@
 import { MapPinLine } from 'phosphor-react'
 import { useTheme } from 'styled-components'
+import { useForm } from 'react-hook-form'
 import {
   CartFormContainer,
   CartFormTitle,
@@ -8,6 +9,7 @@ import {
 } from './styles'
 
 export function CartForm() {
+  const { register } = useForm()
   const { colors } = useTheme()
   return (
     <CartFormContainer>
@@ -17,6 +19,23 @@ export function CartForm() {
           <MapPinLine size={24} color={colors['purple-200']} />
           <span>Endereço de Entrega</span>
           <span>Informe o endereço onde deseja receber seu pedido</span>
+          <form>
+            <input type="text" placeholder="CEP" {...register('CEP')} />
+            <input type="text" placeholder="Rua" {...register('street')} />
+            <input type="text" placeholder="Número" {...register('number')} />
+            <input
+              type="text"
+              placeholder="Complemento"
+              {...register('complemento')}
+            />
+            <input
+              type="text"
+              placeholder="Bairro"
+              {...register('neighborhood')}
+            />
+            <input type="text" placeholder="cidade" {...register('city')} />
+            <input type="text" placeholder="UF" {...register('UF')} />
+          </form>
         </FormAreaTitle>
       </FormAreaContainer>
     </CartFormContainer>
