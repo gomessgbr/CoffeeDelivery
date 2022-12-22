f4ad0c1602b61d2d521f2a63a6cd70d3597e6197
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-22 14:26:51 -0300

    style: adjusts on styling

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,19 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
   margin-bottom: 9.8rem;
+  h1 {
+    margin-left: 10rem;
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    font-weight: 800;
+    line-height: 2.6rem;
+    color: ${({ theme }) => theme.colors['gray-700']};
+  }
+`
+
+export const CoffeesContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10rem 0 10rem 10rem;
+  gap: 2rem;
 `
