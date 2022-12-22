8c124315713cb33b7b705ebf5aa8bc88f4ea7bfe
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-22 12:03:01 -0300

    style: CartContainer style

--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const CartContainer = styled.div``
+export const CartContainer = styled.div`
+  margin-top: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`
