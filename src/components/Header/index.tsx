82e5437480b24dfd45e4d1e9f1e297b7f7340fc2
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-26 20:09:14 -0300

    feat: add count off itens on cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
-import { HeaderContainer, LocaleContainer, HeaderNav } from './styles'
+import {
+  HeaderContainer,
+  LocaleContainer,
+  HeaderNav,
+  CountOfItensonCart,
+} from './styles'
 import coffeDeliveryLogo from '../../assets/CoffeeDeliveryLogo.svg'
 import { NavLink } from 'react-router-dom'
 import { ShoppingCart, MapPin } from 'phosphor-react'
 
+import { useCart } from '../../hooks/useCart'
+
 export function Header() {
+  const { cartItems } = useCart()
+  const cartCountToShowOnScreen = cartItems.length
+
   return (
     <HeaderContainer>
       <NavLink to="/">
@@ -16,6 +26,7 @@ export function Header() {
         </LocaleContainer>
         <NavLink to="Cart" title="carrinho">
           <ShoppingCart size={24} color="#C47F17" weight="fill" />
+          <CountOfItensonCart>{cartCountToShowOnScreen}</CountOfItensonCart>
         </NavLink>
       </HeaderNav>
     </HeaderContainer>
