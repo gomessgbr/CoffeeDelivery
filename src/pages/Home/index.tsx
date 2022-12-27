ad6c288ce01968b0af79a9b6cbf997eae7d153b6
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-27 16:47:21 -0300

    feat: change home components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,21 @@
-import { HomeContainer } from './styles'
+import { CoffeesContent, HomeContainer } from './styles'
 
 import { HeaderHome } from './components/HeaderHome'
 
-import { OurCoffees } from './components/OurCoffee'
+// import { OurCoffees } from './components/OurCoffee'
+import { coffees } from '../../data/coffees'
+import { CoffeeCard } from './components/CoffeCards'
 
 export function Home() {
   return (
     <HomeContainer>
       <HeaderHome />
-      <OurCoffees />
+      <h1>Nossos Cafés</h1>
+      <CoffeesContent>
+        {coffees.map((coffee) => {
+          return <CoffeeCard key={coffee.id} coffee={coffee} />
+        })}
+      </CoffeesContent>
     </HomeContainer>
   )
 }
