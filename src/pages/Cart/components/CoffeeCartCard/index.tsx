78614b0f3732069ac5a4061cfcea1361bddde1d1
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-27 16:45:20 -0300

    feat: CoffeCartCard Component

--- src/pages/Cart/components/CoffeeCartCard/index.tsx
+++ src/pages/Cart/components/CoffeeCartCard/index.tsx
@@ -1,7 +1,77 @@
-export function CoffeeCartCard() {
+import { Minus, Plus, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { useCart } from '../../../../hooks/useCart'
+import { CoffeeCartCardContainer } from './styles'
+
+interface CartItemProps {
+  id: number
+  name: string
+  photo: string
+  price: number
+  amount: number
+}
+
+export function CoffeeCartCard({
+  id,
+  name,
+  photo,
+  price,
+  amount,
+}: CartItemProps) {
+  const { changeCartItem, removeCartItem } = useCart()
+  const [totalValue, setTotalValue] = useState(price * amount)
+
+  function handleIncreaseAmount() {
+    changeCartItem(id, 'increase')
+
+    setTotalValue(totalValue + price)
+  }
+
+  function handleDeacreaseAmount() {
+    if (amount > 1) {
+      changeCartItem(id, 'decrease')
+
+      setTotalValue(totalValue - price)
+    } else if (amount >= 1) {
+      alert('Teste')
+    }
+  }
+
+  function handleRemoveCoffe() {
+    removeCartItem(id)
+  }
+  console.log(photo)
+
   return (
-    <div>
-      <div>CartCards</div>
-    </div>
+    <CoffeeCartCardContainer>
+      <img
+        className="coffeImg"
+        src={`/CoffeesImg/${photo}`}
+        alt="Coffee image"
+      />
+
+      <div>
+        <p className="coffeTypeText">{name}</p>
+        <div className="buttonsWrapper">
+          <div>
+            <button onClick={handleDeacreaseAmount}>
+              <Minus weight="bold" />
+            </button>
+            <p className="itemsInCart">{amount}</p>
+            <button onClick={handleIncreaseAmount}>
+              <Plus weight="bold" />
+            </button>
+          </div>
+          <button onClick={handleRemoveCoffe}>
+            <Trash size={16} color="#8047F8" />
+            REMOVER
+          </button>
+        </div>
+      </div>
+      <div>
+        <p>R$</p>
+        <span>{totalValue.toFixed(2)}</span>
+      </div>
+    </CoffeeCartCardContainer>
   )
 }
--- src/pages/Cart/components/CoffeeCartCard/styles.ts
+++ src/pages/Cart/components/CoffeeCartCard/styles.ts
@@ -1,1 +1,11 @@
 import styled from 'styled-components'
+
+export const CoffeeCartCardContainer = styled.div`
+  display: flex;
+
+  align-items: center;
+  position: relative;
+  width: 23rem;
+  height: 5rem;
+  margin-top: 2.5rem;
+`
