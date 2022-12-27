6da4e73aca953e9bbad9daacfe8864088d615051
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-27 18:06:38 -0300

    feat: add context to form submits

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,13 @@
 import { CartForm } from './components/CartForm'
 import { ConfirmCart } from './components/ConfirmCart'
-
 import * as zod from 'zod'
 
+import { useNavigate } from 'react-router-dom'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { CartContainer } from './styles'
+import { useForm, FormProvider } from 'react-hook-form'
+import { useCart } from '../../hooks/useCart'
 
 const confirmOrderSchema = zod.object({
   cep: zod.string().min(1, 'Informe o cep'),
@@ -15,12 +19,32 @@ const confirmOrderSchema = zod.object({
   uf: zod.string().min(2, 'Informe o estado'),
 })
 
+export type ValidateData = zod.infer<typeof confirmOrderSchema>
+
+type ConfirmValidateData = ValidateData
+
 export function Cart() {
+  const confirmForm = useForm<ConfirmValidateData>({
+    resolver: zodResolver(confirmOrderSchema),
+  })
+
+  const { handleSubmit } = confirmForm
+  const navigate = useNavigate()
+  const { cleanCart } = useCart()
+
+  function handleSubmitForm() {
+    navigate('/checkout')
+
+    cleanCart()
+  }
+
   return (
-    <CartContainer>
-      <CartForm />
-      <form action="" onSubmit={}></form>
-      <ConfirmCart />
-    </CartContainer>
+    <FormProvider {...confirmForm}>
+      <CartContainer>
+        <CartForm />
+        <form action="" onSubmit={handleSubmit(handleSubmitForm)}></form>
+        <ConfirmCart />
+      </CartContainer>
+    </FormProvider>
   )
 }
