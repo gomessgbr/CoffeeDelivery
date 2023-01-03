4864d1b3ff8887f144fc0cb69628d43ef126a14f
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-03 15:11:23 -0300

    feat: fetch data of navigation

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,6 +1,9 @@
 import { CheckoutContainer } from './styles'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 export function Checkout() {
+  const location = useLocation()
+  console.log('Vamo ver o que eu tenho nesta merda! kkkkkjj', location)
   return (
     <CheckoutContainer>
       <div>Checkout</div>
