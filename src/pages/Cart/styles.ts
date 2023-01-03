c7a7511c234cbfab845827318eea5b68139ae5e1
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-03 15:10:26 -0300

    style: Cart

--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -2,8 +2,23 @@ import styled from 'styled-components'
 
 export const CartContainer = styled.div`
   height: 90vh;
-  width: 100%;
+
   display: flex;
   flex-direction: row;
+  justify-content: center;
   background-color: aqua;
 `
+export const InputContainer = styled.div``
+
+export const ConfirmOrderContainer = styled.div``
+
+export const FormArea = styled.div``
+
+export const CartTitleArea = styled.strong`
+  font-family: 'Baloo 2';
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 130%;
+`
+
+export const PaymentMethodArea = styled.div``
