4aada790971140efdd6f424d17f66336959654d4
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-04 17:44:43 -0300

    feat: checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,56 @@
-import { CheckoutContainer } from './styles'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { CheckoutContainer, Content, Main, Title } from './styles'
+import { useLocation } from 'react-router-dom'
+import { CurrencyDollarSimple, MapPin, Timer } from 'phosphor-react'
+
+import checkOutImg from '../../assets/checkOutImg.svg'
 
 export function Checkout() {
-  const location = useLocation()
-  console.log('Vamo ver o que eu tenho nesta merda! kkkkkjj', location)
+  const { state } = useLocation()
+
   return (
     <CheckoutContainer>
-      <div>Checkout</div>
+      <Title>
+        <header className="title">
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </header>
+      </Title>
+      <Content>
+        <Main>
+          <section className="clientInformation">
+            <div className="icon">
+              <MapPin size={16} weight="fill" color={'#FFFFFF'} />
+            </div>
+            <p>
+              Entrega em
+              <strong>
+                {state.street}, {state.number} <br />
+              </strong>
+              {state.neighborhood} - {state.city}, {state.uf}
+            </p>
+          </section>
+          <section className="clientInformation">
+            <div className="icon timer">
+              <Timer size={16} weight="fill" color={'#FFFFFF'} />
+            </div>
+            <p className="column">
+              Previsão de entrega
+              <strong>20 min - 30 min</strong>
+            </p>
+          </section>
+          <section className="clientInformation">
+            <div className="icon dollar">
+              <CurrencyDollarSimple size={16} weight="fill" color={'#FFFFFF'} />
+            </div>
+            <p className="column">
+              Pagamento na entrega
+              <strong>{state.paymentMethod}</strong>
+            </p>
+          </section>
+        </Main>
+
+        <img src={checkOutImg} alt="Entrega de moto" />
+      </Content>
     </CheckoutContainer>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,70 @@
 import styled from 'styled-components'
 
 export const CheckoutContainer = styled.div``
+
+export const Title = styled.header`
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 800;
+
+    color: ${({ theme }) => theme.colors['yellow-300']};
+
+    margin-bottom: 2.5rem;
+
+    p {
+      font-family: 'Roboto', sans-serif;
+      font-size: 1.25rem;
+      font-weight: 400;
+
+      color: ${({ theme }) => theme.colors['gray-500']};
+    }
+  }
+`
+
+export const Main = styled.main`
+  height: 16.875rem;
+  width: 32.875rem;
+
+  border: 1px solid ${({ theme }) => theme.colors['purple-300']};
+  border-radius: 6px 36px;
+  padding: 2rem;
+
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors['gray-800']};
+
+  .clientInformation {
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+    margin-bottom: 2.812rem;
+  }
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .icon {
+    width: 2rem;
+    height: 2rem;
+    padding: 0.5rem;
+    background: ${({ theme }) => theme.colors['purple-300']};
+    border-radius: 50%;
+  }
+
+  .timer {
+    background: ${({ theme }) => theme.colors['yellow-200']};
+  }
+
+  .dollar {
+    background: ${({ theme }) => theme.colors['yellow-300']};
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  gap: 6.375rem;
+`
